f0ed5728b3a82acf616d50575636c77a73b3d5a1
Student <student@Makerss-MacBook-Pro.local>
2019-03-06 16:24:37 +0000

    Added bike attr to docking station. Added ability to dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,19 @@
-require 'bike'
+# require 'bike'
 
 class DockingStation
+
+  attr_reader :bike
 
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  # def bike
+  #   @bike
+  # end
 
 end
