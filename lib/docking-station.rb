4aa9821728b28cf7a735386bada3563cf43f9ad0
Student <student@Makerss-MacBook-Pro.local>
2019-03-07 17:46:21 +0000

    Updating the readme file

---
+++ lib/docking-station.rb
@@ -0,0 +1,32 @@
+require_relative 'bike'
+class DockingStation
+  DEFAULT_CAPACITY = 20
+  attr_reader :dock, :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @dock = []
+    @capacity = capacity
+  end
+
+  # updated under the assumption that docks start empty
+  def release_bike
+    fail 'No bikes available' if empty?
+    @bike = @dock.pop
+  end
+
+  def dock(bike)
+    fail 'Dockstation is full' if full?
+    @dock << bike
+    @dock[-1]
+  end
+
+  private
+  def full?
+    @dock.size >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @dock.empty?
+  end
+
+end
